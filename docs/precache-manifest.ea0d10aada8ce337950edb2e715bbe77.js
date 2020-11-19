3ade3387a5f3bf47b4bce8a659f00532df31d458
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-11-19 16:53:11 -0500

    Docs agregado

---
+++ docs/precache-manifest.ea0d10aada8ce337950edb2e715bbe77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88622f64eaea00e0db30cdf91e46cac3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bfe4c481056b995211",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "e915424eeb4a1675bf19",
+    "url": "/static/js/2.7e8e1ec7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.7e8e1ec7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bfe4c481056b995211",
+    "url": "/static/js/main.d28314a9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
